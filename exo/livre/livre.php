c44775d49b866409eeedfad20a6d9772cd5912fd
anthony spiteri <anthonyspiteri@hotmail.fr>
2023-02-13 16:13:11 +0100

    ajout class livre constructor

--- exo/livre/livre.php
+++ exo/livre/livre.php
@@ -1,4 +1,25 @@
 <?php 
 
+class livre{
+    private string $title;
+    private int $numberPages;
+    private $yearPublication;
+    private int $price;
+    private Auteur $auteur;
 
+    public function __construct(string $title, int $numberPages, $yearPublication, int $price, Auteur $auteur){
+        $this-> title = $title;
+        $this-> numberPages = $numberPages;
+        $this-> yearPublication = $yearPublication;
+        $this-> price = $price;
+        $this-> auteur = $auteur;
+    }
+
+    
+
+
+
+
+
+}
 ?>
