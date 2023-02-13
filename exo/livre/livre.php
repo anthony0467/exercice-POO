fef4c13c070f55945abc2275d93d8bc96839dc17
anthony spiteri <anthonyspiteri@hotmail.fr>
2023-02-13 16:22:10 +0100

    class livre getter et setter

--- exo/livre/livre.php
+++ exo/livre/livre.php
@@ -15,9 +15,49 @@ class livre{
         $this-> auteur = $auteur;
     }
 
+    //SET
+    public function set_title(string $title){
+        $this-> title = $title;
+    }
+
+    public function set_numberPages(int $numberPages){
+        $this-> numberPages = $numberPages;
+    }
+
+    public function set_yearPublication( $yearPublication){
+        $this-> yearPublication = $yearPublication;
+    }
     
+    public function set_price(int $price){
+        $this-> price = $price;
+    }
+
+    public function set_auteur(Auteur $auteur){
+        $this-> auteur = $auteur;
+    }
 
 
+    //GET
+
+    public function get_title(){
+       return  $this-> title;
+    }
+
+    public function get_numberPages(){
+        return $this-> numberPages;
+    }
+
+    public function get_yearPublication(){
+        return $this-> yearPublication;
+    }
+
+    public function get_price(){
+        return $this-> price;
+    }
+
+    public function get_auteur(){
+        return $this-> auteur;
+    }
 
 
 
