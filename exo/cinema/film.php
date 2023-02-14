ef851718122306c78da6e7c2308115063b66092b
anthony <anthonyspiteri@hotmail.fr>
2023-02-14 16:14:53 +0100

    creation dossier cinema

---
+++ exo/cinema/film.php
@@ -0,0 +1,12 @@
+<?php
+class film{
+    private string $title;
+    private string $date;
+    private int $duree;
+    private Realisateur $realisateur;
+    private string $synopsys;
+    private string $genre;
+
+
+}
+ ?>
---
+++ exo/cinema/realisateur.php
@@ -0,0 +1,53 @@
+<?php 
+class Realisateur{
+    private string $nom;
+    private string $prenom;
+    private string $sexe;
+    private string $dateNaissance;
+
+    public function __construct(string $prenom, string $nom, string $sexe, string $dateNaissance){
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->sexe = $sexe;
+        $this->dateNaissance = $dateNaissance;
+
+    }
+
+    //SET
+
+    public function set_nom(string $nom){
+        $this->nom = $nom;
+    }
+
+    public function set_prenom(string $prenom){
+        $this->nom = $prenom;
+    }
+
+    public function set_sexe(string $sexe){
+        $this->sexe = $sexe;
+    }
+
+    public function set_dateNaissance(string $dateNaissance){
+        $this->dateNaissance = $dateNaissance;
+    }
+
+    //GET
+
+    public function get_nom(){
+        return $this->nom;
+    }
+
+    public function get_prenom(){
+        return $this->prenom;
+    }
+
+    public function get_sexe(){
+        return $this->sexe;
+    }
+
+    public function get_dateNaissance(){
+        return $this->dateNaissance;
+    }
+
+}
+?>
